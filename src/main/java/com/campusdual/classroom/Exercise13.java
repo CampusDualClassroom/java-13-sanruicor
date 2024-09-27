0797df9f289d4a9cbc5c83f09f5b18fd226706ff
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-09-27 14:41:25 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -5,7 +5,9 @@ public class Exercise13 {
         FuelTank ft = new FuelTank();
         ft.showDetails();
         System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+//        ft.actualFuel = -8;
+        ft.setActualFuel(-8);
+        ft.setActualFuel(25);
         ft.showDetails();
     }
 }
